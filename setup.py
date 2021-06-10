5ebafecf7f404fde0ba32fcb4b7549976f41bcef
Alan Stockwell <alan@decodeit.co.nz>
2021-06-11 09:34:57 +1200

    convert to pip installable layout

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='deckboxdiff',
+    version='0.1.1',
+    author='Alan Stockwell',
+    author_email='alan.stockwell@gmail.com',
+    description='Helper classes and functions for deriving the difference between two deckbox exports',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alanstockwell/deckboxdiff',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'python-dateutil',
+        'openpyxl',
+        'pandas',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
